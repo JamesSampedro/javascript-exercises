7cf99e0d835f693fa01de0f8d1d42e68f0a56216
James Paul Sampedro <jamez.8833@gmail.com>
2024-06-12 21:32:47 +0800

    finished all the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(string, num) {
     let word = "";
+    if (num < 0) return "ERROR";
     for (let i = 0; i < num; i++) {
         word += string;
     }
@@ -7,6 +8,9 @@ const repeatString = function(string, num) {
 };
 repeatString("hey", 3);
 repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
