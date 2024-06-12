b557696b92b718bb8ed0f4780934a4641d049d5b
James Paul Sampedro <jamez.8833@gmail.com>
2024-06-12 12:02:21 +0800

    print hey 3 times using loop repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function() {
-
+    let string = "";
+    for (let num = 0; num < 3; num++) {
+        string += "hey";
+    }
+    return string;
 };
 
 // Do not edit below this line
